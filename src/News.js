d9176374fca2f643ca7ab7b2f62d802927e607b3
CanniJr <cannigialaluw@gmail.com>
2021-01-11 18:32:29 -0500

    First commit

--- src/News.js
+++ src/News.js
@@ -1,25 +1,42 @@
-import React, { useEffect, useState } from 'react'
+import React, { Component, useEffect, useState } from 'react'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import SingleNews from './SingleNews.js'
+import Home from './Home'
 
-function News() {
-    const [news, setNews] = useState([])
+class News extends Component {
+   state = {
+       news: []
+   }
+    
+    componentDidMount() {
+        const url = 'http://newsapi.org/v2/everything?domains=wsj.com&apiKey=69a513d0971f40a48bb5c1f87c28b250'
+        fetch(url)
+            .then(resp => resp.json())
+            .then(data => this.setState({ news: data }))
+            .catch(console.error());
+    }
 
-    useEffect(() => {
-        const url = 'http://newsapi.org/v2/everything?domains=wsj.com&apiKey=69a513d0971f40a48bb5c1f87c28b250';
 
-        const fetchData = async () => {
-            const result = await fetch(url)
-                .then(resp => resp.json());
-            setNews({ result });
-        }
-        fetchData();
-    }, []);
+    clickHandler = () => {
+        console.log(this.state.news)
+    }
 
-    return (
-        <div className='news'>
-            {news.map(newObj => <SingleNews />)}
-        </div>
-    )
+    render() {
+        return (
+            <Switch>
+                <Router>
+                    <Route path='/news'>
+                        <div className='news'>
+                            <button onClick={this.clickHandler}>Click me!</button>
+                        </div>
+                    </Route>
+                    <Route path='/home'>
+                        <Home />
+                    </Route>
+                </Router>
+            </Switch>
+        )
+    }
 }
 
 export default News
